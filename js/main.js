02032f72a8c8001fada86cddf003b53d3bb0d0d1
AnnaNguyen205 <156358256+AnnaNguyen205@users.noreply.github.com>
2024-12-03 16:17:24 -0500

    AJAX Final
    In this commit, the AJAX request was called successfully, along with the loading animation.

--- js/main.js
+++ js/main.js
@@ -1,76 +1,89 @@
 (() => {
   //variables
   const hotspots = document.querySelectorAll(".Hotspot");
+  const hotspotAnnotation = document.querySelectorAll(".HotspotAnnotation");
+
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
+  const errorContainer = document.querySelector("#error-container");
 
-  //This information needs to be removed then pulled with an AJAX Call using the Fetch API
+  // Loader
+  const loader = document.querySelector("#loader");
+
+  // Material
   //this is the api url https://swiftpixel.com/earbud/api/infoboxes"
-
-  //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/materials"
 
-  //functions
+  //Fnctions
   function loadInfoBoxes() {
-    //make AJAX call here
-
+    //AJAX Call
     fetch("https://swiftpixel.com/earbud/api/infoboxes")
       .then((response) => response.json())
       .then((infoBoxes) => {
-        console.log(infoBoxes);
+        infoBoxes.forEach((infoBox, index) => {
+          let selected = document.querySelector(`#hotspot-${index + 1}`);
 
-        // infoBoxes.forEach((infoBox, index) => {
-        //   let selected = document.querySelector(`#hotspot-${index + 1}`);
+          const titleElement = document.createElement("h2");
+          titleElement.textContent = infoBox.heading;
 
-        //   const titleElement = document.createElement("h2");
-        //   titleElement.textContent = infoBox.title;
+          const textElement = document.createElement("p");
+          textElement.textContent = infoBox.description;
 
-        //   const textElement = document.createElement("p");
-        //   textElement.textContent = infoBox.text;
+          // Image
+          const thumbElement = document.createElement("img");
+          thumbElement.src = `images/earbud${index + 1}.jpg`;
+          thumbElement.alt = infoBox.heading;
 
-        //   selected.appendChild(titleElement);
-        //   selected.appendChild(textElement);
-        // });
+          selected.appendChild(thumbElement);
+          selected.appendChild(titleElement);
+          selected.appendChild(textElement);
+        });
       })
-      .catch();
+      // Error Message
+      .catch((error) => {
+        console.log(error);
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent =
+          "Oops, something went wrong. Please check your internet connection or try again later.";
 
-    infoBoxes.forEach((infoBox, index) => {
-      let selected = document.querySelector(`#hotspot-${index + 1}`);
-
-      const titleElement = document.createElement("h2");
-      titleElement.textContent = infoBox.title;
-
-      const textElement = document.createElement("p");
-      textElement.textContent = infoBox.text;
-
-      selected.appendChild(titleElement);
-      selected.appendChild(textElement);
-    });
+        hotspotAnnotation.forEach((annotation) => {
+          annotation.appendChild(errorMessage.cloneNode(true));
+        });
+      });
   }
   loadInfoBoxes();
 
   function loadMaterialInfo() {
-    fetch("https://swiftpixel.com/earbud/api/materials")
+    fetch("https://swiftpixel.com/earbud/api/materialsxxx")
       .then((response) => response.json())
-      .then((materials) => {
-        console.log(materials);
-
-        materials.forEach((material) => {
-          // clone the template
+      .then((materialListData) => {
+        materialListData.forEach((material) => {
+          // Clone the template
           const clone = materialTemplate.content.cloneNode(true);
-          // populate with data
+
+          // Populate template
           const materialHeading = clone.querySelector(".material-heading");
           materialHeading.textContent = material.heading;
 
-          const materialDescription = clone.querySelector(
+          const paragraphDescription = clone.querySelector(
             ".material-description"
           );
-          materialDescription.textContent = material.description;
+          paragraphDescription.textContent = material.description;
 
           materialList.appendChild(clone);
         });
+
+        loader.classList.toggle("hidden");
       })
-      .catch();
+
+      .catch((error) => {
+        console.log(error);
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent =
+          "Oops, something went wrong. Please check your internet connection or try again later.";
+
+        errorContainer.appendChild(errorMessage);
+      });
   }
   loadMaterialInfo();
 
